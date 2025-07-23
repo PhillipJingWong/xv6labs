337aea1c236b63bea96116ab9afd31cc3bfaf18b
Phillip <phillipjingwong@gmail.com>
2025-07-24 00:36:16 +0100

    Lab 6

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -49,7 +49,7 @@ e1000_init(uint32 *xregs)
     panic("e1000");
   regs[E1000_TDLEN] = sizeof(tx_ring);
   regs[E1000_TDH] = regs[E1000_TDT] = 0;
-  
+
   // [E1000 14.4] Receive initialization
   memset(rx_ring, 0, sizeof(rx_ring));
   for (i = 0; i < RX_RING_SIZE; i++) {
@@ -84,7 +84,7 @@ e1000_init(uint32 *xregs)
     E1000_RCTL_BAM |                 // enable broadcast
     E1000_RCTL_SZ_2048 |             // 2048-byte rx buffers
     E1000_RCTL_SECRC;                // strip CRC
-  
+
   // ask e1000 for receive interrupts.
   regs[E1000_RDTR] = 0; // interrupt after every received packet (no timer)
   regs[E1000_RADV] = 0; // interrupt after every packet (no timer)
@@ -102,8 +102,49 @@ e1000_transmit(char *buf, int len)
   // a pointer so that it can be freed after send completes.
   //
 
-  
-  return 0;
+  //printf("Hello world\n");
+
+/*
+  uint32 tdt = regs[E1000_TDT];
+  struct tx_desc *desc = &tx_ring[tdt];
+  if((desc->status & E1000_TXD_STAT_DD)==0){
+    return -1;
+  }else if(tx_bufs[tdt]){
+    kfree(tx_bufs[tdt]);
+    tx_bufs[tdt] =0;
+  }
+  desc->addr = (uint64)buf;
+  desc->length=len;
+  desc->cmd=E1000_TXD_CMD_EOP;
+  desc->status=0;
+
+  tx_bufs[tdt]=buf;
+
+  regs[E1000_TDT] = (tdt+1) % TX_RING_SIZE;
+
+  return 0;*/
+
+
+    acquire(&e1000_lock);
+      int tx_index = regs[E1000_TDT];
+      if (!(tx_ring[tx_index].status & E1000_TXD_STAT_DD)) {
+        release(&e1000_lock);
+        return -1;
+      }
+
+      // use kfree() to free the last buffer that was transmitted from that descriptor (if there was one)
+      if (tx_bufs[tx_index])
+        kfree(tx_bufs[tx_index]);
+
+      tx_bufs[tx_index] = buf;
+      tx_ring[tx_index].addr = (uint64) buf;
+      tx_ring[tx_index].length = len;
+      tx_ring[tx_index].cmd = E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP;
+      tx_ring[tx_index].status  = 0;
+      regs[E1000_TDT] = (tx_index + 1) % TX_RING_SIZE;
+      release(&e1000_lock);
+      return 0;
+
 }
 
 static void
@@ -115,6 +156,85 @@ e1000_recv(void)
   // Check for packets that have arrived from the e1000
   // Create and deliver a buf for each packet (using net_rx()).
   //
+/*
+  //ring index of next waiting received packet
+
+  //get the rdt register which is what was previously sent
+  uint32 rdt = regs[E1000_RDT];
+  //increment to get what needs to be sent next
+  uint32 next_rdt= (rdt+1) % RX_RING_SIZE;
+  //get the descriptor for this
+  struct rx_desc *desc = &rx_ring[next_rdt];
+
+  //keep looping as long as the dd bit is set
+  while(desc->status & E1000_RXD_STAT_DD){
+
+  //pointer to area to be filled in buffer
+  char *buf =rx_bufs[next_rdt];
+  //length of the data
+  int len=desc->length;
+  //send to the os network stack
+  net_rx(buf, len);
+
+  //allocate memory
+  rx_bufs[next_rdt]=kalloc();
+  //exit if failed allocation
+  if(rx_bufs[next_rdt]==0)
+    return;
+  //reset the status to 0
+  desc->status=0;
+
+  //move along to the next
+  next_rdt=(next_rdt+1) % RX_RING_SIZE;
+}
+
+  regs[E1000_RDT]=(next_rdt-1 +RX_RING_SIZE) % RX_RING_SIZE;*/
+
+/*
+  // [E1000 14.4] Receive initialization
+  memset(rx_ring, 0, sizeof(rx_ring));
+  for (i = 0; i < RX_RING_SIZE; i++) {
+    rx_bufs[i] = kalloc();
+    if (!rx_bufs[i])
+      panic("e1000");
+    rx_ring[i].addr = (uint64) rx_bufs[i];
+  }
+  regs[E1000_RDBAL] = (uint64) rx_ring;
+  if(sizeof(rx_ring) % 128 != 0)
+    panic("e1000");
+  regs[E1000_RDH] = 0;
+  regs[E1000_RDT] = RX_RING_SIZE - 1;
+  regs[E1000_RDLEN] = sizeof(rx_ring);*/
+
+
+    //printf("Bye world\n");
+
+
+  while(1) {
+      int rx_tail = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+      int rx_head = regs[E1000_RDH];
+
+      // check if the buffer is full
+      if(rx_tail == rx_head)
+        return;
+      if(!(rx_ring[rx_tail].status & E1000_RXD_STAT_DD))
+        return;
+      if(rx_ring[rx_tail].errors) {
+        printf("e1000: rx error\n");
+        return;
+      }
+
+      if(rx_ring[rx_tail].status & E1000_RXD_STAT_EOP) {
+        net_rx(rx_bufs[rx_tail], rx_ring[rx_tail].length);
+        rx_bufs[rx_tail] = kalloc();
+        if(!rx_bufs[rx_tail])
+          panic("e1000");
+        rx_ring[rx_tail].addr = (uint64)rx_bufs[rx_tail];
+        rx_ring[rx_tail].status = 0;
+      }
+
+      regs[E1000_RDT] = rx_tail;
+    }
 
 }
 
--- kernel/net.c
+++ kernel/net.c
@@ -10,6 +10,53 @@
 #include "file.h"
 #include "net.h"
 
+//custom declarations
+/*
+#define MAX_PKTS 16
+
+typedef enum {false, true} bool;
+
+//keep track bound ports and store received in a buffer
+bool *bound[255];
+//char * portbuffer[255];
+
+void *portbuffer[255][16];
+int next_packet[255];
+int packet_count[255];
+
+int head[255];
+int tail[255];
+int count[255];
+
+struct packet{
+  char data[1600];
+  int len;
+};
+
+struct spinlock portlock[255];*/
+
+static struct spinlock udptablelock;
+
+ #define PORT_MAX 65536
+ #define WAIT_MAX 17
+
+ struct queue{
+   int head;
+   int tail;
+   char *buf[WAIT_MAX];
+   int size[WAIT_MAX];
+   uint32 src_ip[WAIT_MAX];
+   uint16 src_port[WAIT_MAX];
+ };
+
+ struct binded_port {
+   struct spinlock queuelock;
+   struct queue q;
+   int binded;
+ };
+
+ static struct binded_port udptable[PORT_MAX];
+
 // xv6's ethernet and IP addresses
 static uint8 local_mac[ETHADDR_LEN] = { 0x52, 0x54, 0x00, 0x12, 0x34, 0x56 };
 static uint32 local_ip = MAKE_IP_ADDR(10, 0, 2, 15);
@@ -23,8 +70,45 @@ void
 netinit(void)
 {
   initlock(&netlock, "netlock");
+
+  initlock(&udptablelock, "udptablelock");
 }
 
+
+ int
+ enqueue(struct queue *q, char *buf, int size, uint32 src_ip, uint16 src_port)
+ {
+   if ((q->tail + 1) % WAIT_MAX == q->head)
+     return -1;
+   q->buf[q->tail] = buf;
+   q->size[q->tail] = size;
+   q->src_ip[q->tail] = src_ip;
+   q->src_port[q->tail] = src_port;
+   q->tail = (q->tail + 1) % WAIT_MAX;
+   return 0;
+ }
+
+ void
+ dequeue(struct queue *q)
+ {
+   if (q->head == q->tail)
+     return ;
+   q->head = (q->head + 1) % WAIT_MAX;
+ }
+
+ int
+ isempty(struct queue *q)
+ {
+   if (q->head == q->tail)
+     return 1;
+   else return 0;
+ }
+
+ int
+ queue_size(struct queue *q)
+ {
+   return (q->tail - q->head + WAIT_MAX) % WAIT_MAX;
+ }
 
 //
 // bind(int port)
@@ -37,8 +121,54 @@ sys_bind(void)
   //
   // Your code here.
   //
+/*
+  int port=0;
+  argint(0, &port);
 
-  return -1;
+  if(port>255 || port<0)
+    return -1;
+
+  *bound[port]=true;
+
+  //create buffer for that port if it doesnt exist
+  for(int i=0;i< 16;i++){
+    portbuffer[port][i]=kalloc();
+    if(!portbuffer[port][i]){
+      return -1;
+    }
+  }
+
+  count[port]=0;
+  head[port]=0;
+  tail[port]=0;
+
+  return 0;*/
+  int port;
+     argint(0, &port);
+
+     // port = ntohs(port);
+     printf("bind: port = %d\n", port);
+
+     if (port < 0 || port >= PORT_MAX) {
+     printf("bind: port out of range\n");
+     return -1;
+   }
+
+   acquire(&udptablelock);
+   if (udptable[port].binded == 1) {
+     release(&udptablelock);
+     return -1;
+   }
+
+   udptable[port].binded = 1;
+   initlock(&udptable[port].queuelock, "queuelock");
+   udptable[port].q.head = 0;
+   udptable[port].q.tail = 0;
+   udptable[port].binded = 1;
+
+   release(&udptablelock);
+   return 0;
+
 }
 
 //
@@ -53,7 +183,27 @@ sys_unbind(void)
   // Optional: Your code here.
   //
 
-  return 0;
+  int port;
+    argint(0, &port);
+
+    if (port < 0 || port >= PORT_MAX) {
+      printf("unbind: port out of range\n");
+      return -1;
+    }
+
+    acquire(&udptablelock);
+    if (udptable[port].binded == 0) {
+      printf("unbind: port not binded\n");
+      release(&udptablelock);
+      return -1;
+    }
+
+    udptable[port].binded = 0;
+    udptable[port].q.head = 0;
+    udptable[port].q.tail = 0;
+    release(&udptablelock);
+
+    return 0;
 }
 
 //
@@ -77,8 +227,134 @@ sys_recv(void)
   //
   // Your code here.
   //
+
+/*
+  int dport;
+  uint64 src;
+  uint64 sport;
+  uint64 buf;
+  int maxlen;
+
+      argint(0, &dport);
+      argaddr(1, &src);
+      argaddr(2, &sport);
+      argaddr(3, &buf);
+      argint(4, &maxlen);
+
+    acquire(&portlock[dport]);
+
+    while(count[dport]==0){
+      sleep((void*)portbuffer[dport], &portlock[dport]);
+    }
+
+
+  //copy from kernel to user process
+  //copyout;
+
+  //free packets copied out or dropped
+
+  //struct packet *pkt = &portbuffer[dport][head[dport]];
+  struct packet *pkt = (struct packet *)&portbuffer[dport][head[dport]];
+
+  char *kbuf = pkt->data;
+
+  //get all the headers and payload
+  struct eth *eth= (struct eth *)(kbuf);
+  struct ip *ip= (struct ip *)(eth+1);
+  struct udp *udp= (struct udp *)(ip+1);
+  char *payload = (char *)(udp+1);
+
+  int payload_len = pkt->len - sizeof(struct eth) - sizeof(struct ip) - sizeof(struct udp);
+  int copylen = payload_len < maxlen ? payload_len : maxlen;
+
+  struct proc *p=myproc();
+
+if (copyout(p->pagetable, (uint64)buf, payload, copylen) < 0 ||
+    copyout(p->pagetable, (uint64)sport, (char *)&udp->sport, sizeof(short)) < 0 ||
+    copyout(p->pagetable, (uint64)src, (char *)&ip->ip_src, sizeof(int)) < 0) {
+  release(&portlock[dport]);
   return -1;
 }
+
+head[dport] = (head[dport] + 1) % MAX_PKTS;
+count[dport]--;
+
+release(&portlock[dport]);
+
+  return copylen;
+}
+*/
+
+int dport;
+   uint64 srcaddr;
+   uint64 sportaddr;
+   uint64 bufaddr;
+   int maxlen;
+
+   struct proc* p = myproc();
+
+   argint(0, &dport);
+   argaddr(1, &srcaddr);
+   argaddr(2, &sportaddr);
+   argaddr(3, &bufaddr);
+   argint(4, &maxlen);
+
+
+   printf("recv: dport = %d\n", dport);
+
+   if (dport < 0 || dport >= PORT_MAX) {
+     printf("recv: port out of range\n");
+     return -1;
+   }
+
+   acquire(&udptablelock);
+   if (udptable[dport].binded == 0) {
+     printf("recv: port not binded\n");
+     release(&udptablelock);
+     return -1;
+   }
+
+   struct binded_port *bp = &udptable[dport];
+
+   acquire(&bp->queuelock);
+   release (&udptablelock);
+
+   while (isempty(&bp->q)) {
+     if (p->killed) {
+       release(&bp->queuelock);
+       return -1;
+     }
+     sleep(&bp->q, &bp->queuelock);
+   }
+
+   int len = 0;
+
+   if (srcaddr != 0) {
+     if (copyout(p->pagetable, srcaddr, (char *)&bp->q.src_ip[bp->q.head], sizeof(bp->q.src_ip[bp->q.head])) < 0) {
+       goto bad;
+     }
+   }
+
+   if (sportaddr != 0) {
+     if (copyout(p->pagetable, sportaddr, (char *)&bp->q.src_port[bp->q.head], sizeof(bp->q.src_port[bp->q.head])) < 0) {
+       goto bad;
+     }
+   }
+
+   len = bp->q.size[bp->q.head];
+   if (len > maxlen)
+     len = maxlen;
+
+   if (copyout(p->pagetable, bufaddr, bp->q.buf[bp->q.head], len) < 0) {
+     goto bad;
+   }
+
+ bad:
+   kfree(bp->q.buf[bp->q.head]);
+   dequeue(&bp->q);
+   release(&bp->queuelock);
+   return len;
+ }
 
 // This code is lifted from FreeBSD's ping.c, and is copyright by the Regents
 // of the University of California.
@@ -191,7 +467,99 @@ ip_rx(char *buf, int len)
   //
   // Your code here.
   //
-  
+
+  //receive udp packets, queue them, allow user processes to read them
+
+  //decide if arriving packet is UDP and is dest port is binded
+  //if true save the packet for recv can see it
+  //drop if more than 16 packets saved for each invidual port
+
+/*
+  struct eth *eth= (struct eth *)(buf);
+  struct ip *ip= (struct ip *)(eth+1);
+
+  //check if udp
+  if(ip->ip_p != 17){
+    return;
+  }
+
+  struct udp *udp= (struct udp *)(ip+1);
+  if(*bound[udp->dport] == false){
+    return;
+  }
+  //since the port is binded, save the packet for recv to see it
+  //use a circular buffer, if full then drop
+  if(packet_count[udp->dport] < 16){
+      portbuffer[udp->dport][next_packet[udp->dport]] = buf;
+      packet_count[udp->dport]++;
+      next_packet[udp->dport]=(next_packet[udp->dport]+1)%16;
+  }
+    else return;*/
+
+    struct eth *ineth = (struct eth *) buf;
+      struct ip *inip = (struct ip *)(ineth + 1);
+
+      // if (inip->ip_p != IPPROTO_UDP)
+      //   return;
+      if (len < sizeof(struct eth) + sizeof(struct ip) + sizeof(struct udp)) {
+        printf("ip_rx: packet too short for udp\n");
+        return;
+      }
+
+      struct udp *inudp = (struct udp *)(inip + 1);
+
+      int udp_len = ntohs(inudp->ulen);
+
+      if (udp_len < sizeof(struct udp) ||
+          len < sizeof(struct eth) + sizeof(struct ip) + udp_len)
+        return;
+
+      uint16 dport = ntohs(inudp->dport);
+      printf("ip_rx: dport after ntohs = %d\n", dport);
+
+      acquire(&udptablelock);
+      if (udptable[dport].binded == 0) {
+        printf("ip_rx: port not binded\n");
+        kfree(buf);
+        release(&udptablelock);
+        return;
+      }
+      struct binded_port *bp = &udptable[dport];
+      acquire (&bp->queuelock);
+      if (queue_size(&bp->q) == WAIT_MAX - 1) {
+        printf("ip_rx: queue full\n");
+        release(&bp->queuelock);
+        kfree(buf);
+        release(&udptablelock);
+        return;
+      }
+
+      release(&udptablelock);
+
+      // copy the payload to a new buffer
+      int payload_len = udp_len - sizeof(struct udp);
+      char *payload = (char *)(inudp + 1);
+
+      char *payload_buf = kalloc();
+      if (payload_buf == 0) {
+        printf("ip_rx: kalloc failed\n");
+        goto bad;
+      }
+
+      memmove(payload_buf, payload, payload_len);
+      if (enqueue(&bp->q, payload_buf, payload_len, ntohl(inip->ip_src), ntohs(inudp->sport)) < 0) {
+        printf("ip_rx: queue full\n");
+        kfree(payload_buf);
+        goto bad;
+      }
+      wakeup(&bp->q);
+
+    bad:
+      release(&bp->queuelock);
+      kfree(buf); // free the original packet
+
+
+
 }
 
 //
@@ -219,7 +587,7 @@ arp_rx(char *inbuf)
   char *buf = kalloc();
   if(buf == 0)
     panic("send_arp_reply");
-  
+
   struct eth *eth = (struct eth *) buf;
   memmove(eth->dhost, ineth->shost, ETHADDR_LEN); // ethernet destination = query source
   memmove(eth->shost, local_mac, ETHADDR_LEN); // ethernet source = xv6's ethernet address
